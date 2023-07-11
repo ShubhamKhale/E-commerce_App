8c5c24dfa1f8d6af82617289310fff2347da3249
Shubham <shubhamkhale301@gmail.com>
2023-07-11 14:09:36 +0530

    added controllers routes routes and middlewares

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+
 
 dotenv.config();
 
@@ -15,9 +17,15 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(morgan('dev'));
+
+//routes 
+app.use('/api/v1/auth', authRoutes);
+
+//rest api 
 app.get("/", (req, res) => {
     res.send("<h1>hello</h1>");
 });
+
 
 const PORT = process.env.PORT || 8080;
 
