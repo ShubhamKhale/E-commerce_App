f13e3298670eb46abfc72a7a13210f2ba6c78e86
Shubham <shubhamkhale301@gmail.com>
2023-07-11 12:21:33 +0530

    mongodb setup done

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import  express  from "express";
 import colors from "colors";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+//database config
+connectDB();
+
+//rest object
 const app = express();
 
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'));
 app.get("/", (req, res) => {
     res.send("<h1>hello</h1>");
 });
